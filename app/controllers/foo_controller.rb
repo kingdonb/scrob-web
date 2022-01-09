a67647787b901b630c43e230da1e6cbb0846f9f3
Kingdon Barrett <kingdon@weave.works>
2022-01-09 09:37:12 -0500

    Get a spreadsheet in my terminal
    Signed-off-by: Kingdon Barrett <kingdon@weave.works>

--- app/controllers/foo_controller.rb
+++ app/controllers/foo_controller.rb
@@ -1,4 +1,8 @@
+require './lib/ecoli/spreadsheet'
+
 class FooController < ApplicationController
   def bar
+    s = Ecoli::Spreadsheet.new(google_sheet_id: '1vAoOl4xCSPJcIK95FYdZ9C9Q-zxBn-1mLCyEhnVOaBU')
+    binding.pry
   end
 end
--- lib/ecoli/spreadsheet.rb
+++ lib/ecoli/spreadsheet.rb
@@ -7,9 +7,9 @@ module Ecoli
     extend Forwardable
     def_delegators :@properties, :[], :[]=
 
-    def initialize(google_sheet_id:, scrub_event_id:, previous_event_id:, csvinput_filename:, discussion_csvinput_filename: nil)
+    def initialize(google_sheet_id:)
       @spreadsheet_id = google_sheet_id
-      @config = Config.new(input_file: csvinput_filename, discussion_input_file: discussion_csvinput_filename)
+      @config = Config.new()
       session = GoogleDrive::Session.from_config(@config.config_json)
 
       @properties = {
@@ -18,11 +18,15 @@ module Ecoli
       }
 
       @properties[:ws] =
-        session.spreadsheet_by_key(spreadsheet_id).worksheet_by_title("Raw data")
+        session.spreadsheet_by_key(spreadsheet_id) #.worksheet_by_title("Raw data")
     end
 
     def pry
       binding.pry
+    end
+
+    def spreadsheet_id
+      @spreadsheet_id
     end
   end
 end
