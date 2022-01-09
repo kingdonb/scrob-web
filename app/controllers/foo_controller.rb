4592045e90b30ede44611cb4935374ecf5f367d8
Kingdon Barrett <kingdon@weave.works>
2022-01-09 16:37:29 -0500

    working situation to inspect and magic each data
    (magic remains forthcoming)

--- app/controllers/foo_controller.rb
+++ app/controllers/foo_controller.rb
@@ -1,8 +1,13 @@
 require './lib/ecoli/spreadsheet'
+require 'ap'
 
 class FooController < ApplicationController
   def bar
     @s = Ecoli::Spreadsheet.new(google_sheet_id: '1vAoOl4xCSPJcIK95FYdZ9C9Q-zxBn-1mLCyEhnVOaBU')
     @w = @s.all_worksheets
+
+    @records = @w[2].records.ai(html: true)
+    @title = @w[2].ws_title
+    @ok = @w[2].ok?
   end
 end
