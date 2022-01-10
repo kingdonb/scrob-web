6dc535b0a751e63c7eab80a9ba8eb381c4f41344
Kingdon Barrett <kingdon@weave.works>
2022-01-09 20:43:44 -0500

    test up to year 2021 - current dataset

--- app/controllers/foo_controller.rb
+++ app/controllers/foo_controller.rb
@@ -6,7 +6,7 @@ class FooController < ApplicationController
     @s = Ecoli::Spreadsheet.new(google_sheet_id: '1vAoOl4xCSPJcIK95FYdZ9C9Q-zxBn-1mLCyEhnVOaBU')
     @w = @s.all_worksheets
 
-    n = 22
+    n = 30  # year = 2021
     @records  = @w[n].records.ai(html: true)
     @title    = @w[n].ws_title
     @ok       = @w[n].ok?
