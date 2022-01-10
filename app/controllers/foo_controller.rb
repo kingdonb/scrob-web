bd846b9ce9faebf8f994f9d1662386e9e1440078
Kingdon Barrett <kingdon@weave.works>
2022-01-09 22:43:49 -0500

    let's make some CSV

--- app/controllers/foo_controller.rb
+++ app/controllers/foo_controller.rb
@@ -9,6 +9,8 @@ class FooController < ApplicationController
     @n_ok = @s.ok_worksheets.count
     @ok_worksheets = @s.ok_worksheets.map(&:ws_title)
 
+    @csv = @s.to_csv
+
     n = 30  # year = 2021
     # @records  = @w[n].records.ai(html: true)
     # @title    = @w[n].ws_title
--- lib/ecoli/spreadsheet.rb
+++ lib/ecoli/spreadsheet.rb
@@ -44,6 +44,7 @@ module Ecoli
     end
 
     def ok_worksheets
+      @ok_worksheets ||=
       all_worksheets.map do |worksheet|
         if worksheet.ok?
           worksheet
@@ -51,6 +52,18 @@ module Ecoli
           nil
         end
       end.compact
+    end
+
+    def to_csv
+      o = ok_worksheets
+      r = ok_worksheets.map(&:records)
+      f = r.flatten.map{|s| Record.new(s)}
+      csv_string = CSV.generate do |csv|
+        csv << f.first.known_keys # header row
+        rows = f.each do |record|
+          csv << record.to_csv
+        end
+      end
     end
   end
 end
