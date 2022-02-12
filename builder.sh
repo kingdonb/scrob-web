8f11897463c564425beb29bd415cc938a0337917
Kingdon Barrett <kingdon@weave.works>
2022-02-12 13:08:01 -0500

    for CI, I'd like to keep it this way for now

--- builder.sh
+++ builder.sh
@@ -4,4 +4,4 @@ bundle exec kuby -e production dockerfiles --only app > manifests/Dockerfile
 bundle exec kuby -e production dockerfiles --only assets > manifests/Dockerfile.assets
 
 # bundle exec kuby -e production resources | minus secrets
-bundle exec ruby split-builder.rb # > outputs to - manifests/k8s.yml
+bundle exec ruby builder.rb # > outputs to - manifests/k8s.yml
