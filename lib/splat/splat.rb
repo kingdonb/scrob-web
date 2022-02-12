248c8017231fdfbea27e5594b9c82018b9f7e458
Kingdon Barrett <kingdon@weave.works>
2022-02-12 13:04:59 -0500

    I think this will work

--- lib/splat/splat.rb
+++ lib/splat/splat.rb
@@ -1,16 +1,24 @@
 # typed: strict
 require 'forwardable'
 
-require 'pry'
+# require 'pry'
 
 module Splat
   module_function
   def sort_into_namespaces(arr)
-    binding.pry
+    arr.group_by {|o| o.dig("metadata", "namespace")}
   end
 
   def write_namespaces_into_folder(namespaces_arr:, target_dir:)
-    binding.pry
+    namespaces_arr.each do |namespace, arr|
+      path = File.join(target_dir,namespace)
+      FileUtils.mkdir_p path
+      arr.each do |o|
+        object_name = o.dig("metadata", "name")
+        target_file = File.join(path, "#{object_name}.yaml")
+        File.open(target_file, 'w') { |file| file.write(o.to_yaml) }
+      end
+    end
   end
 end
 
