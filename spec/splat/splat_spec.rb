e767dda30d60dfc2739e8408fcdacf4a0ad05084
Kingdon Barrett <kingdon@weave.works>
2022-02-12 13:53:58 -0500

    (red): the spec is wrong but
    it covers most or all of the logic we've written so far, and one edge
    case that we didn't (which we're about to fix right now!)

---
+++ spec/splat/splat_spec.rb
@@ -0,0 +1,63 @@
+require './lib/splat/splat'
+# Splat.write_namespaces_into_folder(
+#   target_dir: './secrets',
+#   namespaces_arr: Splat.sort_into_namespaces(secrets))
+
+require 'pry'
+
+RSpec.describe Splat do
+  describe '#sort_into_namespaces' do
+    it 'groups by namespace' do
+      t = [{"metadata"=> {"namespace"=> "foo"}}, {"metadata"=> {"namespace"=> "bar"}}]
+      r = Splat.sort_into_namespaces(t)
+      expect(r.keys).to eq(['foo', 'bar'])
+    end
+
+    it 'puts unnamespaced things into a separate bin' do
+      t = [{"metadata"=> {"name"=> "foo"}}, {"metadata"=> {"name"=> "bar"}}]
+      r = Splat.sort_into_namespaces(t)
+      expect(r.keys).to eq([nil])
+    end
+  end
+
+  describe '#write_namespaces_into_folder' do
+    context 'namespaced content' do
+      let(:t) {
+        {"foo"=>[{"metadata"=>{"namespace"=>"foo", "name"=>"foo"}}],
+         "bar"=>[{"metadata"=>{"namespace"=>"bar", "name"=>"foo"}}]}
+      }
+      it 'works' do
+        expect(Splat).to receive(:write_to_file).with(
+          :object=>{"metadata"=>{"name"=>"foo", "namespace"=>"foo"}},
+          :target_file=>"test-tmp-dir/foo/foo.yaml"
+        ).ordered
+        expect(Splat).to receive(:write_to_file).with(
+          :object=>{"metadata"=>{"name"=>"foo", "namespace"=>"bar"}},
+          :target_file=>"test-tmp-dir/bar/foo.yaml"
+        ).ordered
+
+        r = Splat.write_namespaces_into_folder(
+          namespaces_arr: t, target_dir: 'test-tmp-dir')
+      end
+    end
+    context 'unnamespaced content' do
+      let(:t) {
+        {nil=>[{"metadata"=>{"name"=>"foo"}}],
+         nil=>[{"metadata"=>{"name"=>"foo"}}]}
+      }
+      it 'works' do
+        expect(Splat).to receive(:write_to_file).with(
+          :object=>{"metadata"=>{"name"=>"foo"}},
+          :target_file=>"test-tmp-dir/foo.yaml"
+        ).ordered
+        expect(Splat).to receive(:write_to_file).with(
+          :object=>{"metadata"=>{"name"=>"foo"}},
+          :target_file=>"test-tmp-dir/foo.yaml"
+        ).ordered
+
+        r = Splat.write_namespaces_into_folder(
+          namespaces_arr: t, target_dir: 'test-tmp-dir')
+      end
+    end
+  end
+end
