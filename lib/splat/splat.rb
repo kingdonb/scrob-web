5f68a7bf1fd0c1d7342b907dd28f00eadfee2a0c
Kingdon Barrett <kingdon@weave.works>
2022-02-12 12:43:19 -0500

    let's implement lib/splat

---
+++ lib/splat/splat.rb
@@ -0,0 +1,13 @@
+# typed: strict
+require 'forwardable'
+
+module Splat
+  def sort_into_namespaces(arr)
+    binding.pry
+  end
+
+  def write_namespaces_into_folder(namespaces_arr:, target_dir:)
+    binding.pry
+  end
+end
+
