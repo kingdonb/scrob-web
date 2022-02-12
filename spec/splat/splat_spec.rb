4cf135b4b9751d4f16f6cdc4fd87ab5e714a83fe
Kingdon Barrett <kingdon@weave.works>
2022-02-12 14:36:05 -0500

    update the spec (module Splat)
    I mentioned earlier that the spec was wrong:
    
    It's supposed to put files into namespace folders according to object
    name and kind, so we can have objects with the same name but different
    kinds in the same namespace (this is something I think Kuby did, so if
    a spec does not implement this as well, we might lose some resources...)

--- spec/splat/splat_spec.rb
+++ spec/splat/splat_spec.rb
@@ -19,40 +19,52 @@ RSpec.describe Splat do
   end
 
   describe '#write_namespaces_into_folder' do
-    context 'namespaced content' do
+    context 'namespaced content missing Kind' do
       let(:t) {
         {"foo"=>[{"metadata"=>{"namespace"=>"foo", "name"=>"foo"}}],
          "bar"=>[{"metadata"=>{"namespace"=>"bar", "name"=>"foo"}}]}
       }
+      it 'fails' do
+        expect {
+          Splat.write_namespaces_into_folder(
+            namespaces_arr: t, target_dir: 'test-tmp-dir')
+        }.to raise_error(StandardError, "Kubernetes YAML resources cannot be missing apiVersion or kind")
+      end
+    end
+    context 'namespaced content, properly formed' do
+      let(:t) {
+        {"foo"=>[{"metadata"=>{"namespace"=>"foo", "name"=>"foo"}, "apiVersion"=>"v1", "kind"=>"Whatever"}],
+         "bar"=>[{"metadata"=>{"namespace"=>"bar", "name"=>"foo"}, "apiVersion"=>"v1", "kind"=>"Whatever"}]}
+      }
       it 'works' do
         allow(Splat).to receive(:mkdirp)
         expect(Splat).to receive(:write_to_file).with(
-          :object=>{"metadata"=>{"name"=>"foo", "namespace"=>"foo"}},
-          :target_file=>"test-tmp-dir/foo/foo.yaml"
+          :object=>{"metadata"=>{"name"=>"foo","namespace"=>"foo"},"apiVersion"=>"v1","kind"=>"Whatever"},
+          :target_file=>"test-tmp-dir/foo/foo-whatever.yaml"
         ).ordered
         expect(Splat).to receive(:write_to_file).with(
-          :object=>{"metadata"=>{"name"=>"foo", "namespace"=>"bar"}},
-          :target_file=>"test-tmp-dir/bar/foo.yaml"
+          :object=>{"metadata"=>{"name"=>"foo","namespace"=>"bar"},"apiVersion"=>"v1","kind"=>"Whatever"},
+          :target_file=>"test-tmp-dir/bar/foo-whatever.yaml"
         ).ordered
 
         r = Splat.write_namespaces_into_folder(
           namespaces_arr: t, target_dir: 'test-tmp-dir')
       end
     end
-    context 'unnamespaced content' do
+    context 'unnamespaced content, properly formed' do
       let(:t) {
-        {nil=>[{"metadata"=>{"name"=>"foo"}},
-               {"metadata"=>{"name"=>"bar"}}]}
+        {nil=>[{"metadata"=>{"name"=>"foo"}, "apiVersion"=>"v1", "kind"=>"Whatever"},
+               {"metadata"=>{"name"=>"bar"}, "apiVersion"=>"v1", "kind"=>"Whatever"}]}
       }
       it 'works' do
         allow(Splat).to receive(:mkdirp)
         expect(Splat).to receive(:write_to_file).with(
-          :object=>{"metadata"=>{"name"=>"foo"}},
-          :target_file=>"test-tmp-dir/foo.yaml"
+          :object=>{"metadata"=>{"name"=>"foo"},"apiVersion"=>"v1","kind"=>"Whatever"},
+          :target_file=>"test-tmp-dir/foo-whatever.yaml"
         ).ordered
         expect(Splat).to receive(:write_to_file).with(
-          :object=>{"metadata"=>{"name"=>"bar"}},
-          :target_file=>"test-tmp-dir/bar.yaml"
+          :object=>{"metadata"=>{"name"=>"bar"},"apiVersion"=>"v1","kind"=>"Whatever"},
+          :target_file=>"test-tmp-dir/bar-whatever.yaml"
         ).ordered
 
         r = Splat.write_namespaces_into_folder(
