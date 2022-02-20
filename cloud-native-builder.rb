2bcd8d25ef671b4a36af0a3253060dd912e0fac6
Kingdon Barrett <kingdon@weave.works>
2022-02-20 16:43:16 -0500

    (dangerous) but this Procfile worked
    Force the development environment to start with 0.0.0.0
    (Warning! Dangerous)
    
    With a little known trick called a...procfile? Whoops... what's old is
    always new again. I'm so glad to see I can still use this in Kubernetes.
    
    Signed-off-by: Kingdon Barrett <kingdon@weave.works>

--- cloud-native-builder.rb
+++ cloud-native-builder.rb
@@ -2,9 +2,10 @@
 
 APP_ENV=development
 APP_NAME=scrob-web
-APP_PORT=9292
+APP_PORT=5001
+APP_HOST=0.0.0.0
 APP_PACKER=pack
-BUILDPACK=https://cnb-shim.herokuapp.com/v1/yebyen/og-pack-multi
+BUILDPACK=https://cnb-shim.herokuapp.com/v1/heroku/ruby
 BUILDER=heroku/buildpacks:20
 
 echo "To build the app:"
@@ -12,4 +13,5 @@ echo "$APP_PACKER build $APP_NAME --buildpack $BUILDPACK \\
   --builder $BUILDER"
 echo
 echo "To start the app in dev mode:"
-echo "docker run -e DATABASE_URL='nulldb://nohost' -e RAILS_ENV=$APP_ENV -e RAILS_LOG_TO_STDOUT=true -p $APP_PORT:$APP_PORT -it $APP_NAME"
+echo "docker run -e DATABASE_URL='nulldb://nohost' -e RAILS_ENV=$APP_ENV -e HOST=$APP_HOST \\
+  -e RAILS_LOG_TO_STDOUT=true -e PORT=$APP_PORT -p $APP_PORT:$APP_PORT -it $APP_NAME"
