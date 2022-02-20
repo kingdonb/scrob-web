7ad0a1abcf78354326d97062a2d919137876569c
Kingdon Barrett <kingdon@weave.works>
2022-02-20 15:58:42 -0500

    (failed) use the ruby buildpack with cnb pack
    The fix I applied to use with nulldb apparently backfired, build fails
    with an error about not being able to list rake tasks. (But I can list
    rake tasks... are you sure it's not a specific rake task you wanted?)
    
    Signed-off-by: Kingdon Barrett <kingdon@weave.works>

--- cloud-native-builder.rb
+++ cloud-native-builder.rb
@@ -4,12 +4,12 @@ APP_ENV=development
 APP_NAME=scrob-web
 APP_PORT=9292
 APP_PACKER=pack
-BUILDPACK=gcr.io/paketo-buildpacks/ruby
-BUILDER=paketobuildpacks/builder:base
+BUILDPACK=https://cnb-shim.herokuapp.com/v1/heroku/ruby
+BUILDER=heroku/buildpacks:20
 
 echo "To build the app:"
 echo "$APP_PACKER build $APP_NAME --buildpack $BUILDPACK \\
   --builder $BUILDER"
 echo
 echo "To start the app in dev mode:"
-echo "docker run -e RAILS_ENV=$APP_ENV -e RAILS_LOG_TO_STDOUT=true -p $APP_PORT:$APP_PORT -it $APP_NAME"
+echo "docker run -e DATABASE_URL='nulldb://nohost' -e RAILS_ENV=$APP_ENV -e RAILS_LOG_TO_STDOUT=true -p $APP_PORT:$APP_PORT -it $APP_NAME"
