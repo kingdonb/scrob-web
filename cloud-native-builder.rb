ecd73710673b105c0d03a37cbeb482e03959acf3
Kingdon Barrett <kingdon@weave.works>
2022-02-20 16:57:49 -0500

    get rid of some noise
    this dangerous thing is buried away safely in the Procfile now, it has
    defaults there now so need not be exposed in the frontend to our cloud
    native pack experience.
    
    Signed-off-by: Kingdon Barrett <kingdon@weave.works>

--- cloud-native-builder.rb
+++ cloud-native-builder.rb
@@ -3,7 +3,7 @@
 APP_ENV=development
 APP_NAME=scrob-web
 APP_PORT=5001
-APP_HOST=0.0.0.0
+# APP_HOST=0.0.0.0
 APP_PACKER=pack
 BUILDPACK=https://cnb-shim.herokuapp.com/v1/heroku/ruby
 BUILDER=heroku/buildpacks:20
@@ -13,5 +13,6 @@ echo "$APP_PACKER build $APP_NAME --buildpack $BUILDPACK \\
   --builder $BUILDER"
 echo
 echo "To start the app in dev mode:"
-echo "docker run -e DATABASE_URL='nulldb://nohost' -e RAILS_ENV=$APP_ENV -e HOST=$APP_HOST \\
-  -e RAILS_LOG_TO_STDOUT=true -e PORT=$APP_PORT -p $APP_PORT:$APP_PORT -it $APP_NAME"
+echo "docker run -e DATABASE_URL='nulldb://nohost' -e RAILS_ENV=$APP_ENV \\
+  -e RAILS_LOG_TO_STDOUT=true -p $APP_PORT:$APP_PORT -it $APP_NAME"
+# -e HOST=$APP_HOST -e PORT=$APP_PORT \\ 
