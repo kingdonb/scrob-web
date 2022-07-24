0808e7efbee619ef51dc46a5df89644f9f394900
Kingdon Barrett <kingdon@weave.works>
2022-07-24 15:52:12 -0400

    try to make something happen in foo controller
    Signed-off-by: Kingdon Barrett <kingdon@weave.works>

--- app/controllers/foo_controller.rb
+++ app/controllers/foo_controller.rb
@@ -3,15 +3,18 @@ require 'ap'
 
 class FooController < ApplicationController
   def bar
-    @s = Ecoli::Spreadsheet.new(google_sheet_id: '1vAoOl4xCSPJcIK95FYdZ9C9Q-zxBn-1mLCyEhnVOaBU')
-    @w = @s.all_worksheets
+    @f = "Foo Bar"
 
-    @n_ok = @s.ok_worksheets.count
-    @ok_worksheets = @s.ok_worksheets.map(&:ws_title)
 
-    @csv = @s.to_csv
+    # @s = Ecoli::Spreadsheet.new(google_sheet_id: '1vAoOl4xCSPJcIK95FYdZ9C9Q-zxBn-1mLCyEhnVOaBU')
+    # @w = @s.all_worksheets
 
-    n = 30  # year = 2021
+    # @n_ok = @s.ok_worksheets.count
+    # @ok_worksheets = @s.ok_worksheets.map(&:ws_title)
+
+    # @csv = @s.to_csv
+
+    # n = 30  # year = 2021
     # @records  = @w[n].records.ai(html: true)
     # @title    = @w[n].ws_title
     # @ok       = @w[n].ok?
