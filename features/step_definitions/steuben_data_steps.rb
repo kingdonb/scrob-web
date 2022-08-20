2d1f1ee257ee864d3a52a6b9be6ce1e530611969
Kingdon Barrett <kingdon@weave.works>
2022-08-20 13:29:11 -0400

    add first Cucumber feature
    Signed-off-by: Kingdon Barrett <kingdon@weave.works>

---
+++ features/step_definitions/steuben_data_steps.rb
@@ -0,0 +1,43 @@
+Given('we start from the webpage where the data is kept updated') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('we have downloaded a spreadsheet full of many tabs of mostly similar data') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('the site name is located on a numbered tab') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('the numbered tabs all contain different variables in each row') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('the dates are column headers') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('the data is gathered from the many tabs into one list of records') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('the tabs labeled {string} and {string} can be ignored') do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('each record in the list is written into the output spreadsheet') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('each column represents a variable, an observation date, or the site name') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('each row represents an observation of one or more of the variables') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('the site name is added to the end of each row in an additional column') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
